8b90b4bd93393d3095a0caf762c539602181c0ac
CodeZaru <teamsullivank@gmail.com>
2018-01-23 15:32:32 -0500

    'files'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,10 +4,14 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => (
     <div>
-    <div style={{ backgroundImage: 'url(./jumbo.jpg)' }}>
-        <Jumbotron className="text-center"  style={{ backgroundImage: 'url(./jumbo.jpg)' }}>
-            <h1>NYT Article Search</h1>
-            <p>Search for and save articles of interest</p>
+  <div className="imageTest" style={{ backgroundImage: 'url(http://i.telegraph.co.uk/multimedia/archive/01946/new-york-times_1946907c.jpg)' }}></div>
+    
+    <div className="imageTest" style={{ backgroundImage: 'url(http://i.telegraph.co.uk/multimedia/archive/01946/new-york-times_1946907c.jpg)' }}>
+        <Jumbotron className="text-center" style={{ backgroundImage: 'url(https://shroudofturin.files.wordpress.com/2015/03/image25.png)' }}>
+            <h1></h1>
+            <br></br>
+            <br></br>
+            <p></p>
         </Jumbotron>
     </div>
         <Nav bsStyle="tabs">
